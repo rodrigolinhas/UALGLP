a3080dfabc642928f5659db755edf3b171ffe634
Rodrigo Linhas <rodrigolinhas18@gmail.com>
2025-04-09 18:39:52 +0100

    init final proj

---
+++ TD2/PROJ/proj.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+
+// Necessario 2 sorts
+/**
+ * oredenação alfabetica    : insertion sort
+ * ordenação numerica       : quick sort
+ */
+
+int main(int argc, char const *argv[]){
+    /* code */
+    return 0;
+}
+ 
